28c52d2a1d1df87e2e9074cd22a4034ca644214d
Gabriel Lima <gabriel.lima.ads@gmail.com>
2012-06-16 21:26:33 -0300

    adicionado metodo para retornar data inicio e fim da semana fix #9

--- src/util/UtGeral.java
+++ src/util/UtGeral.java
@@ -3,6 +3,8 @@ package util;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class UtGeral {
@@ -45,5 +47,20 @@ public class UtGeral {
     
     public static String getDataComHora(String data) throws ParseException{  
     	return getDataGeral(data, 2); 
+    }
+    
+    public static Date[] getPrimeiroUltimoDiaDaSemana(){
+    	Calendar dataAtual = Calendar.getInstance();
+    	Calendar primeiroDia = Calendar.getInstance();
+    	Calendar ultimoDia = Calendar.getInstance();
+		int diaDaSemana = dataAtual.get(Calendar.DAY_OF_WEEK);
+		int diaAtual = dataAtual.get(Calendar.DAY_OF_MONTH);
+		int primeiroDiaInt = diaAtual - (diaDaSemana - 1);
+		primeiroDia.set(Calendar.DAY_OF_MONTH, primeiroDiaInt);
+		ultimoDia.set(Calendar.DAY_OF_MONTH, (primeiroDiaInt + 6));
+		Date[] retorno = new Date[2];
+		retorno[0] = primeiroDia.getTime();
+		retorno[1] = ultimoDia.getTime();
+		return retorno;
     }
 }
