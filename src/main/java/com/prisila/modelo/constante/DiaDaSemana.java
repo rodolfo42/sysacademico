efec011b99b4968268531320016640fe08641c3f
Rodolfo Ferreira <rodolfo@rodolfo42.net>
2012-11-20 19:56:13 -0300

    ajuste de encoding do nome do dia da semana

--- src/main/java/com/prisila/modelo/constante/DiaDaSemana.java
+++ src/main/java/com/prisila/modelo/constante/DiaDaSemana.java
@@ -4,11 +4,11 @@ public enum DiaDaSemana {
 	
 	DOMINGO("Domingo",1), 
 	SEGUNDAFEIRA("Segunda-Feira",2), 
-	TERCAFEIRA("Ter�a-Feira",3), 
+	TERCAFEIRA("Terça-Feira",3), 
 	QUARTAFEIRA("Quarta-Feira",4), 
 	QUINTAFEIRA("Quinta-Feira",5), 
 	SEXTAFEIRA("Sexta-Feira",6), 
-	SABADO("S�bado",7);
+	SABADO("Sábado",7);
 	
 	private final String nome;
 	private final int codigo;
