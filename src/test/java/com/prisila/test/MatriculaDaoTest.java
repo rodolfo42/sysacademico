31f8e37ecafffc5616daff69118b0f3251a6d54a
Rodolfo Ferreira <rodolfo@rodolfo42.net>
2012-12-02 22:09:31 -0300

    melhoria nos testes de dao

--- src/test/java/com/prisila/test/MatriculaDaoTest.java
+++ src/test/java/com/prisila/test/MatriculaDaoTest.java
@@ -1,66 +1,42 @@
 package com.prisila.test;
 
-import java.util.Arrays;
-import java.util.Calendar;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import com.prisila.modelo.constante.TipoAula;
-import com.prisila.modelo.entidade.Aluno;
-import com.prisila.modelo.entidade.Curso;
+import com.prisila.dao.AlunoDao;
+import com.prisila.dao.CursoDao;
+import com.prisila.dao.MatriculaDao;
+import com.prisila.dao.ResponsavelDao;
 import com.prisila.modelo.entidade.Matricula;
-import com.prisila.modelo.entidade.Responsavel;
 
-public class MatriculaDaoTest extends DAOTest {
+public class MatriculaDAOTest extends DAOTest {
+	
+	MatriculaDao matriculaDao;
+	AlunoDao alunoDao;
+	ResponsavelDao responsavelDao;
+	private CursoDao cursoDao;
 	
 	@Override
 	public void setup() {
 		super.setup();
+		// TODO usar injecao de dependencia do VRaptor
+		// a questão é: COMO?
+		alunoDao = new AlunoDao(session);
+		matriculaDao = new MatriculaDao(session, alunoDao);
+		responsavelDao = new ResponsavelDao(session);
+		cursoDao = new CursoDao(session);
 	}
 	
 	@Test
 	public void cadastrarMatricula() {
-		Matricula matr = new Matricula();
-		matr.setAluno(getAlunoTeste());
-		matr.setAtivo(true);
-		matr.adicionaVinculo(TipoAula.PRATICA_INDIVIDUAL);
-		matr.adicionaVinculo(TipoAula.PRATICA_GRUPO);
-		matr.setCurso(getCursoTeste());
-		matr.setData(Calendar.getInstance());
-		matr.setResponsavel(getResponsavelTeste());
-	}
-	
-	private Curso getCursoTeste() {
-		Curso curso = new Curso();
-		curso.setNome("Curso teste");
-		return curso;
-	}
-	
-	private Responsavel getResponsavelTeste() {
-		Responsavel resp = new Responsavel();
-		resp.setNome("Responsavel teste");
-		resp.setEmail("teste@exemplo.com");
-		resp.setTelefone("3142532374");
-		resp.setCelular("11998534848");
-		resp.setCep("49100000");
-		resp.setCpf("39784435039");
-		resp.setDataConfirmacao(Calendar.getInstance().getTime());
-		return resp;
-	}
-	
-	private Aluno getAlunoTeste() {
-		Aluno a = new Aluno();
-		a.setNome("Aluno Teste");
-		Calendar dataNasc;
-		dataNasc = Calendar.getInstance();
-		dataNasc.add(Calendar.YEAR, -20);
-		a.setDataNascimento(dataNasc.getTime());
-		a.setListaResponsavel(Arrays.asList(getResponsavelTeste()));
-		return a;
-	}
-	
-	@Override
-	public void finalize() {
-		super.finalize();
+		Matricula matr = TesteDB.newMatricula();
+		
+		cursoDao.salvar(matr.getCurso());
+		responsavelDao.salvar(matr.getResponsavel());
+		alunoDao.salvar(matr.getAluno());
+		
+		matriculaDao.salvar(matr);
+		assertEquals(1, matriculaDao.listaTudo().size());
 	}
 }
--- src/test/java/com/prisila/test/TesteDB.java
+++ src/test/java/com/prisila/test/TesteDB.java
@@ -1,10 +1,17 @@
 package com.prisila.test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 
+import com.prisila.modelo.constante.TipoAula;
+import com.prisila.modelo.entidade.Aluno;
+import com.prisila.modelo.entidade.Curso;
+import com.prisila.modelo.entidade.Matricula;
 import com.prisila.modelo.entidade.Professor;
+import com.prisila.modelo.entidade.Responsavel;
 
 /**
  * Classe que providencia objetos com dados falsos para os test cases
@@ -28,6 +35,53 @@ public class TesteDB {
 		return prof;
 	}
 	
+	public static Curso newCurso() {
+		Curso curso = new Curso();
+		curso.setNome("Curso teste");
+		addInstance(curso);
+		return curso;
+	}
+	
+	public static Responsavel newResponsavel() {
+		Responsavel resp = new Responsavel();
+		resp.setNome("Responsavel teste");
+		resp.setEmail("teste@exemplo.com");
+		resp.setTelefone("3142532374");
+		resp.setCelular("11998534848");
+		resp.setCep("49100000");
+		resp.setCpf("39784435039");
+		resp.setDataConfirmacao(Calendar.getInstance().getTime());
+		addInstance(resp);
+		return resp;
+	}
+	
+	public static Aluno newAluno() {
+		Aluno a = new Aluno();
+		a.setNome("Aluno Teste");
+		
+		Calendar dataNasc;
+		dataNasc = Calendar.getInstance();
+		dataNasc.add(Calendar.YEAR, -20);
+		
+		a.setDataNascimento(dataNasc.getTime());
+		a.setListaResponsavel(Arrays.asList(newResponsavel()));
+		addInstance(a);
+		return a;
+	}
+	
+	public static Matricula newMatricula() {
+		Matricula matr = new Matricula();
+		matr.setAluno(newAluno());
+		matr.setAtivo(true);
+		matr.adicionaVinculo(TipoAula.PRATICA_INDIVIDUAL);
+		matr.adicionaVinculo(TipoAula.PRATICA_GRUPO);
+		matr.setCurso(newCurso());
+		matr.setData(Calendar.getInstance());
+		matr.setResponsavel(matr.getAluno().getListaResponsavel().get(0));
+		addInstance(matr);
+		return matr;
+	}
+	
 	/**
 	 * Retorna todas as instâncias cadastradas
 	 * 
@@ -41,7 +95,7 @@ public class TesteDB {
 	
 	public static <T> T getOne(Class<T> clazz) {
 		ArrayList<T> list = (ArrayList<T>) dbMap.get(clazz);
-		if (list != null && list.size() > 0) {
+		if (list != null) {
 			/*
 			 * nao precisa ser list.size() -1, pois Math.random() só retorna <
 			 * 1.0, então o arredondamento sempre vai ser para baixo
